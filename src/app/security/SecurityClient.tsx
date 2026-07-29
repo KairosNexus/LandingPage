b0526eed3884d5302e3e6edf2b63d03ca3549b62
Bolaji5821 <oyedelebolaji84@gmail.com>
2026-07-29 14:53:35 +0100

    Chore: Update security contact email

--- src/app/security/SecurityClient.tsx
+++ src/app/security/SecurityClient.tsx
@@ -85,7 +85,7 @@ export function SecurityClient() {
       ],
       contact: {
         title: "Security Contact",
-        value: "kairosnexusglobal@gmail.com",
+        value: "info@kairosnexusglobal.com",
       },
     },
   ];
